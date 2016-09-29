c3ad77e3fd63fbb997f4a1590bf24b2633ea8fa3
jkrvivian <jkrvivian@gmail.com>
2016-09-30 00:28:00 +0800

    add testing files

---
+++ clz.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int clz_iteration(uint32_t x);
+int clz_binary_search(uint32_t x);
+int clz_byte_shift(uint32_t x);
+uint8_t clz_recursive(uint32_t x, int shift);
+uint8_t clz_harley(uint32_t x);
